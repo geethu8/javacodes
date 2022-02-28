73d0ee8bd378123542ba40a1789dd30add76c6d7
java903 <100550562+java903@users.noreply.github.com>
2022-02-28 10:29:05 +0530

    Add files via upload

---
+++ hibernatedemo/target/classes/src/com/demo/Employee.java
@@ -0,0 +1,36 @@
+package com.demo;
+
+public class Employee {
+	
+	private long id;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	private String firstname;
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	private String lastname;
+	private String salary;
+	
+	
+
+}
